20a6063c69338ac3dbd89f72f0a9cdac62362727
Kang Nam <kangnam@Kangs-MBP.fios-router.home>
2018-06-21 16:03:30 -0400

    initial commit

---
+++ Kims Cleaners/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Kims Cleaners
+//
+//  Created by Kang Nam on 6/21/18.
+//  Copyright © 2018 Kang Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
---
+++ Kims CleanersTests/Kims_CleanersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Kims_CleanersTests.swift
+//  Kims CleanersTests
+//
+//  Created by Kang Nam on 6/21/18.
+//  Copyright © 2018 Kang Nam. All rights reserved.
+//
+
+import XCTest
+@testable import Kims_Cleaners
+
+class Kims_CleanersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
