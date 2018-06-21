da7cf54668e7c39c38617735c7d649ea0e07d16b
Kang Nam <kangnam@Kangs-MBP.fios-router.home>
2018-06-21 16:30:17 -0400

    Extensions added for anchoring UIViews and creating UIColors by rgb values

---
+++ Kims Cleaners/Extensions.swift
@@ -0,0 +1,47 @@
+//
+//  Extensions.swift
+//  Kims Cleaners
+//
+//  Created by Kang Nam on 6/21/18.
+//  Copyright © 2018 Kang Nam. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, left: NSLayoutXAxisAnchor?, right: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, topPadding: CGFloat, leftPadding: CGFloat, rightPadding: CGFloat, bottomPadding: CGFloat, width: CGFloat, height: CGFloat) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: topPadding).isActive = true
+        }
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: leftPadding).isActive = true
+        }
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -rightPadding).isActive = true
+        }
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -bottomPadding).isActive = true
+        }
+        if width != 0 {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        if height != 0 {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+}
+
+extension UIColor {
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
+    }
+    
+}
